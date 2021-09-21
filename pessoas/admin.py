09fd9d14705eee62badf49131d1b4b2efba90718
Camila Massoli <camila.massoli@gmail.com>
2021-09-20 21:51:16 -0300

    subindo projeto

---
+++ pessoas/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from pessoas.models import Pessoas
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display= ('id', 'nome', 'email')
+    list_display_links=('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 2
+
+
+admin.site.register(Pessoas,ListandoPessoas)
